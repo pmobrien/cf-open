158cef06a51c3ea2d01a928254de0a692d35e7fe
Patrick O'Brien <obrienp@outlook.com>
2018-02-24 17:29:24 -0600

    added scors to serializer

--- server/src/main/java/com/pmobrien/cfopen/neo/pojo/serializers/AthleteSerializer.java
+++ server/src/main/java/com/pmobrien/cfopen/neo/pojo/serializers/AthleteSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.pmobrien.cfopen.neo.pojo.Athlete;
+import com.pmobrien.cfopen.neo.pojo.Score;
 import java.io.IOException;
 
 public class AthleteSerializer extends StdSerializer<Athlete> {
@@ -22,6 +23,21 @@ public class AthleteSerializer extends StdSerializer<Athlete> {
     
     generator.writeStringField("competitorId", athlete.getCompetitorId());
     generator.writeStringField("competitorName", athlete.getCompetitorName());
+    
+    generator.writeArrayFieldStart("scores");
+    
+    for(Score score : athlete.getScores()) {
+      generator.writeStartObject();
+      
+      generator.writeNumberField("ordinal", score.getOrdinal());
+      generator.writeNumberField("rank", score.getRank());
+      generator.writeStringField("score", score.getScore());
+      generator.writeStringField("scoreDisplay", score.getScoreDisplay());
+      
+      generator.writeEndObject();
+    }
+    
+    generator.writeEndArray();
     
     generator.writeEndObject();
   }
