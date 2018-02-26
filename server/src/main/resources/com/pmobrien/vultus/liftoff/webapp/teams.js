70df150f215f56a4f29d8cf29981710980580952
Patrick O'Brien <obrienp@outlook.com>
2018-02-25 20:43:01 -0600

    WIP - drag/droppable athletes

--- server/src/main/resources/com/pmobrien/vultus/liftoff/webapp/teams.js
+++ server/src/main/resources/com/pmobrien/vultus/liftoff/webapp/teams.js
@@ -1,1 +1,12 @@
+function allowDrop(event) {
+  event.preventDefault();
+}
 
+function drag(event) {
+  event.dataTransfer.setData("targetId", event.target.id);
+}
+
+function drop(event, element) {
+  event.preventDefault();
+  element.appendChild(document.getElementById(event.dataTransfer.getData("targetId")));
+}
